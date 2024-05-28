b93ce3ce7d155fd5688b7a05c1554595ccb9c761
user <ibrahim42babayev@gmail.com>
2024-05-28 22:14:17 +0300

    #5 try

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,24 +1,25 @@
 from celery import shared_task
-from config import settings
+from django.conf import settings
 import datetime
 import requests
 
 from habits.models import Habits
-
+import json
 
 
 @shared_task
 def send_notification():
     token = settings.TOKEN_BOT
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    url = f"https://api.telegram.org/bot{token}/"
     now = datetime.datetime.now()
     habit = Habits.objects.all()
-
     for habit_item in habit:
-        print("Бот запускается")
         if now.time() <= habit_item.time:
-            print("УРАА")
-            requests.post(url,
-                          data={"chat_id": habit_item.owner.chat_id,
+            print("Бот запустился")
+            data = requests.get(url + "getupdates")
+            data1 = json.loads(data.content)
+            chat_id = data1["result"][0]["message"]["chat"]["id"]
+            requests.post(url+"sendMessage",
+                          data={"chat_id": chat_id,
                                 "text": f'В {habit_item.time} часов вы должны {habit_item.action} в {habit_item.place}'}
                           )
